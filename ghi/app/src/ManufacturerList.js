8fe34a6e345a69e2629d64c6b750f534ddbbaa2a
Derrick Wan <drrckwan.s@gmail.com>
2022-10-25 11:58:02 -0700

    created manufacturer list, linked in nav and app.js, created loadmanufacturers in index.js

---
+++ ghi/app/src/ManufacturerList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function ManufacturerList(props) {
+    console.log(":::props:::", props)
+    return (
+        <table className="table table-striped">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.manufacturers.map(manufacturer => {
+                    return (
+                        <tr key={manufacturer.href}>
+                            <td>{manufacturer.name}</td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    )
+}
+
+export default ManufacturerList
--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -8,3 +8,23 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
+async function loadManufacturers() {
+  const response = await fetch('http://localhost:8100/api/manufacturers/');
+  console.log(":::response:::", response)
+  if (response.ok) {
+    const data = await response.json();
+    root.render(
+      <React.StrictMode>
+        <App manufacturers={data.manufacturers} />
+      </React.StrictMode>
+    );
+    console.log(":::::data::::::", data)
+    console.log(":::::data.man::::::", data.manufacturers)
+
+  } else {
+    console.error(response);
+  }
+}
+loadManufacturers();
