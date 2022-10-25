7706eb0eb1d2efe910473947bd5ca3d4d231b09c
Ally <allygoose93@gmail.com>
2022-10-25 10:24:25 -0700

    api_views completed and the paths are set up in api_urls

--- sales/api/sales_rest/api_urls.py
+++ sales/api/sales_rest/api_urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .api_views import customer_list, sales_person_list, show_sales_person, list_sales
+from .api_views import customer_list, show_customer, sales_person_list, show_sales, show_sales_person, list_sales
 
 urlpatterns = [
     path("sales/", list_sales, name="list_sales"),
-    # path("sales/<int:sale_id>/", sale_detail, name="sale_detail"),
+    path("sales/<int:pk>/", show_sales, name="show_sales"),
     path("sales/person/", sales_person_list, name="sales_person_list"),
     path("sales/person/<int:pk>/", show_sales_person, name="show_sales_person"),
     path("customers/", customer_list, name="customer_list"),
-    # path("customers/<int:customer_id>/", customer_detail, name="customer_detail"),
+    path("customers/<int:pk>/", show_customer, name="show_customer"),
 ]
--- sales/api/sales_rest/api_views.py
+++ sales/api/sales_rest/api_views.py
@@ -28,24 +28,6 @@ class CustomerEncoder(ModelEncoder):
         'id'
     ]
 
-# class SalesRecordEncoder(ModelEncoder):
-#     model = SalesRecord
-#     properties = [
-#         'automobile',
-#         'sales_person',
-#         'customer',
-#         'price',
-#         'id'
-#     ]
-#     def get_extra_data(self,o):
-#         return {
-#             'automobile': o.automobile.vin,
-#             'sales_person': {
-#                 'name': o.sales_person.name,
-#                 'employee_number': o.sales_person.employee_number,
-#             },
-#             'customer': o.customer.name
-#         }
 
 class SalesRecordEncoder(ModelEncoder):
     model=SalesRecord
@@ -63,7 +45,7 @@ class SalesRecordEncoder(ModelEncoder):
     }
 
 
-
+# sales person functions
 
 # Create your views here.
 @require_http_methods(["GET", "POST"])
@@ -120,6 +102,7 @@ def show_sales_person(request, pk):
             status = 404
             )
 
+# customer functions
 @require_http_methods(["GET", "POST"])
 def customer_list(request):
     if request.method == 'GET':
@@ -145,6 +128,34 @@ def customer_list(request):
                 status=400,
             )
 
+
+@require_http_methods(["GET", "PUT", "DELETE"])
+def show_customer(request, pk):
+    if request.method == "GET":
+        customer = Customer.objects.get(id=pk)
+        return JsonResponse(
+            customer,
+            encoder=CustomerEncoder,
+            safe=False,
+        )
+
+    elif request.method == "DELETE":
+        count, _ = Customer.objects.filter(id=pk).delete()
+        return JsonResponse({"deleted": count > 0})
+        
+    else:
+        content = json.loads(request.body)
+        Customer.objects.filter(id=pk).update(**content)
+        customer = Customer.objects.get(id=pk)
+        return JsonResponse(
+            customer,
+            encoder=CustomerEncoder,
+            safe=False
+        )
+
+
+
+# sales record functions
 
 @require_http_methods(["GET", "POST"])
 def list_sales(request):
@@ -188,3 +199,62 @@ def list_sales(request):
             encoder=SalesRecordEncoder,
             safe=False,
         )
+
+
+@require_http_methods(["GET", "DELETE", "PUT"])
+def show_sales(request, pk):
+    if request.method == "GET":     
+        sales = SalesRecord.objects.get(id=pk)
+        return JsonResponse(
+            sales,
+            encoder=SalesRecordEncoder,
+            safe=False,
+        )
+    elif request.method == "DELETE":
+        count, _ = SalesRecord.objects.filter(id=pk).delete()
+        return JsonResponse({"deleted": count > 0}) 
+    else:
+        content = json.loads(request.body)
+        try:
+            if "automobile" in content:
+                vin = content["automobile"]
+                automobile = AutomobileVO.objects.get(vin=vin)
+                content["automobile"] = automobile
+        except AutomobileVO.DoesNotExist:
+            return JsonResponse(
+                {"message": "Automobile does not exist"},
+                status = 404
+            )
+        try:
+            if "sales_person" in content:
+                id = content["sales_person"]
+                sales_person = SalesPerson.objects.get(id=id)
+                content["sales_person"] = sales_person
+        except SalesPerson.DoesNotExist:
+            return JsonResponse(
+            {"message": "Sales Person does not exist"},
+            status = 404
+            )
+        try:
+            if "customer" in content:
+                id = content["customer"]
+                customer = Customer.objects.get(id=id)
+                content["customer"] = customer
+        except Customer.DoesNotExist:
+            return JsonResponse(
+            {"message": "Customer does not exist"},
+            status = 404
+            )
+        try :
+            SalesRecord.objects.filter(id=pk).update(**content)
+            sales_record = SalesRecord.objects.get(id=pk)
+            return JsonResponse(
+            sales_record,
+            encoder=SalesRecordEncoder,
+            safe=False
+            )
+        except SalesRecord.DoesNotExist:
+            return JsonResponse(
+            {"message": "Invalid sales record"},
+            status=400,
+            )
